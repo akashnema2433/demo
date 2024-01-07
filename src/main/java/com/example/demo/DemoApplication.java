1924e2b1681a532407ad8877cb68ea9e60d5d27c
akashnema2433 <akashnema5726@gmail.com>
2024-01-07 23:01:25 +0530

    test hello

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	@PostConstruct
+	public void runImage(){
+		System.out.println("testing done...");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("hello...");
