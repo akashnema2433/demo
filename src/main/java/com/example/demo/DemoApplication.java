101581bd191eea25fea32086725e29233ccbe91e
akashnema2433 <akashnema5726@gmail.com>
2024-01-08 10:47:08 +0530

    test1 commit for testing

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	@PostConstruct
 	public void runImage(){
 		System.out.println("testing done...");
+		System.out.println("test1....");
 	}
 
 	public static void main(String[] args) {
